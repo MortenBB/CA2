78cf381f226f1d00ac845a388fe9d9d004fa7786
Alek <alek2750@gmail.com>
2018-10-02 11:51:29 +0200

    DTO getter & setter & cons

--- src/main/java/Entity/PersonDTO.java
+++ src/main/java/Entity/PersonDTO.java
@@ -10,7 +10,57 @@ package Entity;
  * @author Morten
  */
 public class PersonDTO {
+
+    private int id;
     private String email;
     private String firstname;
     private String lastname;
+
+    public PersonDTO(Person person) {
+        this.email = person.getEmail();
+        this.firstname = person.getFirstname();
+        this.lastname = person.getLastname();
+    }
+
+    public PersonDTO(String email, String firstname, String lastname) {
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public PersonDTO(int id, String email, String firstname, String lastname) {
+        this.id = id;
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
 }
